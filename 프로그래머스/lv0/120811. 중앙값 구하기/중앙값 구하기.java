cfc71df728fb966a31bd38eb211c6faf8e2cc1cd
heen5620 <87135431+heen5620@users.noreply.github.com>
2023-03-25 23:13:43 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.47 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        Arrays.sort(array);
+      
+                return array[array.length / 2];
+        }
+    
+    }
