405f1cea11462b9e8b9a05e7368df913a1d39684
heen5620 <87135431+heen5620@users.noreply.github.com>
2023-03-28 11:05:21 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        int dab = 0;    
+        
+       for(int i= num_list.length-1; i >= 0; i--){
+          answer[dab] = num_list[i] ;
+          dab++;
+       }
+        
+        return answer;
+    }
+}
