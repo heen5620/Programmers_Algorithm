f3f94f8cdd13ad8320e492698dca8e589bba2348
heen5620 <87135431+heen5620@users.noreply.github.com>
2023-04-05 23:41:17 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,20 +1,12 @@
 class Solution {
     public int solution(String str1, String str2) {
-        int str2Length = str2.length();
-        int str1Length = str1.length();
-
-        for (int i = 0; i <= str1Length - str2Length; i++) {
-            boolean found = true;
-            for (int j = 0; j < str2Length; j++) {
-                if (str1.charAt(i + j) != str2.charAt(j)) {
-                    found = false;
-                    break;
-                }
-            }
-            if (found) {
-                return 1;
-            }
+        int answer = 0;
+        if(str1.contains(str2) == true){
+            answer = 1;
         }
-        return 2;
+        else{
+            answer =2;
+        }
+        return answer;
     }
 }
