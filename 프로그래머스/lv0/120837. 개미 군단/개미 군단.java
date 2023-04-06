9e350ffd0523484664350185eeaab29aaf0964a0
heen5620 <87135431+heen5620@users.noreply.github.com>
2023-04-06 22:48:51 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -1,26 +1,14 @@
 class Solution {
     public int solution(int hp) {
-        int answer = 0;
+        int answer = hp / 5;
+        hp%=5;
         
-        int a = hp / 5;
-        int a_1 = hp % 5;
+        answer+= hp/3;
+        hp%=3;
         
-        int b = a_1 / 3;
-        int b_1 = a_1 % 3;
+        answer+= hp/1;
         
-        int c = b_1 / 1;
-        int c_1 = b_1 % 1;
         
-        if(a_1 == 0){
-            answer = a;
-        }
-        else if(b_1 == 0){
-            answer = a+b;
-        }
-        else if(c_1 == 0){
-            answer = a+b+c;
-        }
-          
         return answer;
     }
 }
