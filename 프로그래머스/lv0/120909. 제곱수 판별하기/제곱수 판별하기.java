39ea266c384043bb2befec6f1d7282728f556b1e
heen5620 <87135431+heen5620@users.noreply.github.com>
2023-04-06 23:36:19 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+      double a = Math.sqrt(n); 
+        if(a==(int)a){
+            return 1;
+        }
+        else{
+            return 2;
+        }
+    }
+}
