162c5a8f1b12fb2d58037262cfdc375e1929578e
heen5620 <87135431+heen5620@users.noreply.github.com>
2023-04-10 00:05:58 +0900

    [level 0] Title: 가위 바위 보, Time: 0.03 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String rsp) {
+       
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<rsp.length();i++){
+            char b = rsp.charAt(i);
+            if(b == '2'){
+                sb.append('0');
+            }
+        else if(b == '0'){
+            sb.append('5');
+        }
+        else if(b =='5'){
+            sb.append('2');
+        }
+        }
+        return sb.toString();
+    }
+}
