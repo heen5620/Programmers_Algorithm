bc7d32ec8e6cbf8bad2ea8bc277fc464005d7f31
heen5620 <87135431+heen5620@users.noreply.github.com>
2023-04-11 22:15:05 +0900

    [level 0] Title: 암호 해독, Time: 14.41 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -1,11 +1,13 @@
 class Solution {
     public String solution(String cipher, int code) {
-        StringBuilder sb = new StringBuilder();
+        String answer = "";
         
-        for(int i=code-1; i < cipher.length(); i+=code){
-            sb.append(cipher.charAt(i));
+        for(int i=0;i<cipher.length();i++){
+            if((1+i) % code == 0){
+                answer+=cipher.charAt(i);
+            }
         }
         
-        return sb.toString();
+        return answer;
     }
 }
