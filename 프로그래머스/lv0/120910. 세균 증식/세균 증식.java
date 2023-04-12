a8b93fdf71c2feeed7e20896dbd7e01cfdf81d36
heen5620 <87135431+heen5620@users.noreply.github.com>
2023-04-12 11:00:11 +0900

    [level 0] Title: 세균 증식, Time: 0.01 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -1,9 +1,11 @@
 class Solution {
     public int solution(int n, int t) {
         int answer = 0;
-          
-        answer = (int)Math.pow(2,t)*n;
         
-        return answer;
+        for(int i=1;i<=t;i++){
+            n = 2*n; 
+        }
+        
+        return n;
     }
 }
