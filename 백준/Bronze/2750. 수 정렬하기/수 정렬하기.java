bb74a97f1eb91d3d03a5021e2810af1012b2bb2d
heen5620 <87135431+heen5620@users.noreply.github.com>
2023-05-03 10:51:01 +0900

    [Bronze II] Title: 수 정렬하기, Time: 352 ms, Memory: 22204 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -5,20 +5,20 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
         int[] array = new int[num]; //길이 num인 배열 생성
-        int temp;
 
         for(int i=0;i<array.length;i++){
             array[i] = scanner.nextInt();
         }
 
-        for(int i=0;i< array.length-1 ;i++){
-            for(int j=0;j< array.length-1-i;j++){
-                if(array[j] > array[j+1]){
-                    temp = array[j+1];
-                    array[j+1] = array[j];
-                    array[j] = temp;
-                }
+        for(int i=0;i< array.length-1;i++){
+            int min_index = i;
+            for(int j=i+1; j< array.length; j++) {
+                if (array[min_index] > array[j])
+                    min_index = j;
             }
+                int temp = array[min_index];
+                array[min_index] = array[i];
+                array[i] = temp;
 
         }
         for (int i : array) {
@@ -27,3 +27,5 @@ public class Main {
     }
 
 }
+
+
