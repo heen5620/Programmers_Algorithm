b3a2cf6ea5d65579d3128ab74cf0f22d92ad4fd5
heen5620 <87135431+heen5620@users.noreply.github.com>
2023-05-18 11:26:12 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(int num) {
         String answer = "";
-        if(num % 2 == 0){
+        if(num%2 == 0){
             answer = "Even";
         }
         else{
