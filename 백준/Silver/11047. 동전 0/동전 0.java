fb660967bd2f5a56b9e54efb96100a9c674571a3
heen5620 <87135431+heen5620@users.noreply.github.com>
2023-05-22 18:28:22 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num = Integer.parseInt(st.nextToken());
+        int price = Integer.parseInt(st.nextToken());
+        int[] arr = new int[num];
+        int count = 0;
+
+        for(int i=0;i<num;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+       for(int i = arr.length-1; i>=0 ;i--){
+           if(arr[i] <= price ){
+               count += price / arr[i];
+               price = price % arr[i];
+           }
+       }
+        System.out.println(count);
+    }
+}
