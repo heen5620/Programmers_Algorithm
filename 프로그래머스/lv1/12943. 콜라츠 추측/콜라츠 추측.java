f5581365e021f188a0a51ba38a15dc26d9ab63c8
heen5620 <87135431+heen5620@users.noreply.github.com>
2023-05-24 19:48:46 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,15 +1,19 @@
 class Solution {
     public int solution(long num) {
         int answer = 0;
-        for(int i=0;i<500;i++){
-            if(num == 1){
-                answer = i;
-                break;
+        while(num!=1){
+            if(num % 2 == 0){
+                num = num / 2;
             }
-            else
-                answer = -1;            
-                num = (num%2==0)?(num/2):(3*num+1);
+            else{
+                num = (num*3)+1;
+            }
+            answer++;
         }
+        if(answer >= 500){
+            answer = -1;
+        }
+        
         return answer;
     }
 }
