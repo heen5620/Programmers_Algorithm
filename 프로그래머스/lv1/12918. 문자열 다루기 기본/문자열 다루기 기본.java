c0d4e80a79887b00f1de2622308992a7c9b8f966
heen5620 <87135431+heen5620@users.noreply.github.com>
2023-06-24 18:12:25 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -2,16 +2,19 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String s) {
-        if (s.length() != 4 && s.length() != 6) {
-            return false; 
+        boolean answer = true;
+        
+        if(s.length() != 4 && s.length() != 6){
+            return false;
         }
         
-        for (char ch : s.toCharArray()) {
-            if (ch < '0' || ch > '9') {
-                return false; 
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch < '0' || ch > '9'){
+                return false;
             }
         }
         
-        return true; 
+        return answer;
     }
 }
