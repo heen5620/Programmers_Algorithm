f7a7bf09dfa6dba417166a2c6b583c9865f3734a
heen5620 <87135431+heen5620@users.noreply.github.com>
2023-09-11 16:29:48 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,17 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		int A = in.nextInt();
-		in.close();
-		
-		if(A>=90) System.out.println("A");
-		else if(A>=80) System.out.println("B");
-		else if (A>=70) System.out.println("C");
-		else if(A>=60) System.out.println("D");
-		else System.out.println("F");
-	}
- 
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int number = Integer.parseInt(br.readLine());
+        if (number >= 90) {
+            System.out.println("A");
+        } else if (number >= 80) {
+            System.out.println("B");
+        } else if (number >= 70) {
+            System.out.println("C");
+        } else if (number >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
 }
